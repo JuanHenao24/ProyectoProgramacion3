943287041801d2ad9f6e86136e761e8989c9e517
Manuela Arcila Cruz <66129053+Manuela0214@users.noreply.github.com>
2020-07-05 09:37:58 -0500

    Update index.ts

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,8 +1,1 @@
 
-export * from './amistad.repository';
-export * from './mensajes.repository';
-export * from './comentario.repository';
-export * from './publicaciones.repository';
-export * from './usuario.repository';
-export * from './etiquetado.repository';
-export * from './videojuego.repository';
